d978bde94e221fe0482366bf0937d7bf738bacdc
vasusriram6 <vasusriram6@gmail.com>
2025-10-13 22:49:44 +0530

    adding test workflows

--- tests/test_class_wafermapdataset.py
+++ tests/test_class_wafermapdataset.py
@@ -9,5 +9,17 @@ def test_wafermapdataset_getitem():
     dataset = WaferMapDataset(test_maps, labels, transform=None)
     for i in range(len(dataset)):
         img, label = dataset[i]
-        assert img.shape[0] in (1, 3) or img.shape[-1] in (1, 3)   # Should be 3 channel
+
+        # assert img.shape[0] in (1, 3) or img.shape[-1] in (1, 3)   # Should be 3 channel
+        # assert isinstance(label, int)
+
+        # Handle PIL Image case
+        if hasattr(img, "mode"):
+            # Should be RGB, size tuple
+            assert img.mode in ["RGB", "L"]
+            assert isinstance(img.size, tuple)
+        else:
+            # If a Tensor (e.g., after ToTensor transform)
+            assert hasattr(img, "shape")
+            assert img.shape[0] in (1, 3) or img.shape[-1] in (1, 3)
         assert isinstance(label, int)
